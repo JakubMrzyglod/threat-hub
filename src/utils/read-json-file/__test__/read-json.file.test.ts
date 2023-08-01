cf0b1acdb2ef2e72e7d494f8ab5bcbcf3140937d
Jakub Mrzyglod <jakub1mrzyglod@gmail.com>
2023-08-01 12:09:37 +0200

    refactor

--- src/utils/read-json-file/__test__/read-json.file.test.ts
+++ src/utils/read-json-file/__test__/read-json.file.test.ts
@@ -1,5 +1,30 @@
+import { readJsonFile } from '../read-json-file.util';
+
 describe(`readJsonFile`, () => {
-  it('should pass', async () => {
-    expect(true).toBeTruthy();
+  it('should read correctly', async () => {
+    const filePath = './example.json';
+    const readPromise = readJsonFile(__dirname, filePath);
+    const originalJsonData = require('./example.json');
+
+    await expect(readPromise).resolves.toEqual(originalJsonData);
+  });
+
+  it('should throw error for empty file', async () => {
+    const filePath = './empty.json';
+    const readPromise = readJsonFile(__dirname, filePath);
+
+    await expect(readPromise).rejects.toEqual(
+      new Error('Could not parse file data for: <empty-file>')
+    );
+  });
+
+  it('should throw error for invalid file path', async () => {
+    const filePath = './invalid-path.json';
+    const readPromise = readJsonFile(__dirname, filePath);
+    const originalJsonData = require('./example.json');
+
+    await expect(readPromise).rejects.toEqual(
+      new Error(`Could not read file for path: [${filePath}]`)
+    );
   });
 });
--- src/utils/read-json-file/read-json-file.util.ts
+++ src/utils/read-json-file/read-json-file.util.ts
@@ -1,13 +1,35 @@
-import { readFile } from 'fs/promises';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 export const readJsonFile = async <R extends Record<string, any>>(
+  dirPath: string,
   filePath: string
 ): Promise<R> => {
+  const json = await readFile(dirPath, filePath);
+  const data = parseJson<R>(json);
+
+  return data;
+};
+
+const readFile = async (dirPath: string, filePath: string): Promise<string> => {
   try {
-    const jsonData = await readFile(filePath, { encoding: 'utf8' });
-    const data = JSON.parse(jsonData) as R;
-    return data;
+    const fullFilePath = path.resolve(dirPath, filePath);
+    const jsonData = await fs.readFile(fullFilePath, { encoding: 'utf8' });
+
+    return jsonData;
   } catch (e) {
     throw new Error(`Could not read file for path: [${filePath}]`);
+  }
+};
+
+const parseJson = <R>(jsonData: string): R => {
+  try {
+    const data = JSON.parse(jsonData);
+
+    return data;
+  } catch (e) {
+    throw new Error(
+      `Could not parse file data for: ${jsonData || '<empty-file>'}`
+    );
   }
 };
